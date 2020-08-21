99a894b5a8193bcf392469c2f194b5d88d9788cb
Santiago Quintana <san3091@gmail.com>
2020-08-21 12:20:16 -0500

    add csv exporter

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,4 +1,25 @@
 class Result < ApplicationRecord
   belongs_to :run
   validates :data, presence: true
+
+  def system
+    data["system"]
+  end
+
+  def subject_information
+    data["subject_information"]
+  end
+
+  def time_in_minutes
+    data["time_in_minutes"]
+  end
+
+  def trials
+    data["trials"]
+  end
+
+  def headers
+    trials.values.first.keys + system.keys + subject_information.keys
+  end
+
 end
--- app/models/run.rb
+++ app/models/run.rb
@@ -1,5 +1,27 @@
+require 'csv'
+
 class Run < ApplicationRecord
   belongs_to :experiment
   has_many :results, dependent: :destroy
   validates :title, presence: true
+
+  def as_csv
+    csv_string = CSV.generate(headers: true) do |csv|
+      # generate headers from the first result
+      csv << results.first.headers
+
+      results.map do |result|
+        # extract common attributes
+        common_attributes = result.system
+          .merge(result.subject_information)
+          .merge({"time_in_minutes" => result.time_in_minutes})
+
+        result.trials.each_value do |trial|
+          row = trial.merge(common_attributes)
+          csv << row.values
+        end
+      end
+    end
+  end
+
 end
